d34e6300caea2d097f610b2833ffc8281ec1df05
broewaa1 <oropeze1@uci.edu>
2024-07-09 07:08:05 -0700

    First customization to Neovim configuration and add Github Copilot
    Here I am trying to add some of the resources I believe I have from attending my current educational program

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,13 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+require('lazy').setup {
+  -- ... other plugins ...
+  {
+    'github/copilot.nvim',
+    event = 'InsertEnter',
+    config = function()
+      vim.g.copilot_enabled = true
+    end,
+  },
+}
